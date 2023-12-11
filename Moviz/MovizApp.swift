7769ae610822e9b2c6a5fc68c7bbc726814826ca
Charlene Hoareau <charlene.hoareau@outlook.com>
2023-12-11 14:33:57 +0100

    Initial Commit

---
+++ Moviz/MovizApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovizApp.swift
+//  Moviz
+//
+//  Created by charlene hoareau on 11/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct MovizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
