3211c39c4cf61cb4b74556d91dd72c5e9f26ac88
Charlene Hoareau <charlene.hoareau@outlook.com>
2023-12-11 15:35:16 +0100

    compose the detail view

--- Moviz/ContentView.swift
+++ Moviz/ContentView.swift
@@ -9,21 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            /*Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)*/
-            Text("Under the Dome")
-                .font(.title)
-            HStack {
-                Text("Drama, Science-Fiction, Thriller")
-                    .font(.subheadline)
-                Spacer()
-                Text("CBS")
-                    .font(.subheadline)
+        VStack {
+            rectangleImage()
+               /* .offset(y: -130)
+                .padding(.bottom, -130)*/
+            
+            VStack(alignment: .leading) {
+                /*Image(systemName: "globe")
+                 .imageScale(.large)
+                 .foregroundColor(.accentColor)*/
+                Text("Under the Dome")
+                    .font(.title)
+                    .fontWeight(.bold)
+                HStack {
+                    Text("Drama, Science-Fiction, Thriller")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("Rating : 6.5")
+                        .font(.subheadline)
+                        .fontWeight(.bold)
+                }
+                HStack {
+                    Text("Thursday (22:00)")
+                    Spacer()
+                    Text("CBS")
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+
+                Divider()
+                Text("About Under the Dome")
+                    .font(.title3)
+                    .fontWeight(.bold)
+                Text("Under the Dome is the story of a small town that is suddenly and inexplicably sealed off from the rest of the world by an enormous transparent dome. The town's inhabitants must deal with surviving the post-apocalyptic conditions while searching for answers about the dome, where it came from and if and when it will go away.")
+                    .font(.body)
             }
+            .padding()
+            Spacer()
+            
         }
-        .padding()
     }
 }
 
