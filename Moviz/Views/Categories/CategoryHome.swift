43cdd60c12b080cef06d133e60bc889f0e61d5e8
Charlene Hoareau <charlene.hoareau@outlook.com>
2023-12-13 10:01:03 +0100

    move content ContentView to MovieList and create CategoryHome

---
+++ Moviz/Views/Categories/CategoryHome.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryHome.swift
+//  Moviz
+//
+//  Created by charlene hoareau on 13/12/2023.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
---
+++ Moviz/Views/Movies/MovieList.swift
@@ -0,0 +1,52 @@
+//
+//  MovieList.swift
+//  Moviz
+//
+//  Created by charlene hoareau on 12/12/2023.
+//
+
+import SwiftUI
+
+struct MovieList: View {
+@EnvironmentObject var dataManager: DataManager
+@State private var showFavoritesOnly = false
+
+var body: some View {
+    NavigationSplitView {
+        Toggle("Show Favorites Only", isOn: $showFavoritesOnly)
+            .padding()
+        List {
+            ForEach(filteredMovies) { movie in
+                NavigationLink(
+                    destination: MovieDetail(movie: movie),
+                    label: {
+                        MovieRow(movie: movie)
+                    }
+                )
+            }
+        }
+        .navigationTitle("Movies")
+    } detail: {
+        Text("Select a Movie")
+    }
+    .onAppear {
+        loadData()
+    }
+}
+
+private var filteredMovies: [Movie] {
+    return showFavoritesOnly ? dataManager.favoriteMovies : dataManager.movies
+}
+
+private func loadData() {
+    dataManager.fetchMovies { _ in }
+}
+}
+
+struct MovieList_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieList()
+            .environmentObject(DataManager())
+    }
+}
+
