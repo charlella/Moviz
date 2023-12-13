267c619c33bf816c6a3879bdf0b103937ef80222
Charlene Hoareau <charlene.hoareau@outlook.com>
2023-12-13 10:35:31 +0100

    CategoryItem

---
+++ Moviz/Views/Categories/CategoryItem.swift
@@ -0,0 +1,39 @@
+//
+//  CategoryItem.swift
+//  Moviz
+//
+//  Created by charlene hoareau on 13/12/2023.
+//
+
+import SwiftUI
+import URLImage
+
+struct CategoryItem: View {
+    @EnvironmentObject var dataManager: DataManager
+    var movie: Movie
+    
+    var body: some View {
+        VStack(alignment: .center) {
+            URLImage(URL(string: movie.image.medium)!) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height: 200)
+                    .cornerRadius(10)
+            }
+                    Text(movie.name)
+                        .foregroundStyle(.primary)
+                        .font(.subheadline)
+                }
+                .padding(.leading, 15)
+    }
+}
+
+struct CategoryItem_Previews: PreviewProvider {
+    static var previews: some View {
+        let dataManager = DataManager()
+        dataManager.movies = ModelData().movies
+        return CategoryItem(movie: dataManager.movies[0])
+            .environmentObject(DataManager())
+    }
+}
---
+++ Moviz/Views/Categories/CategoryRow.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryRow.swift
+//  Moviz
+//
+//  Created by charlene hoareau on 13/12/2023.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryRow_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryRow()
+    }
+}
